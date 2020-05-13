b77eca7a92246309d1b9397ab04f17afb6be4757
novalb12 <novalb65@gmail.com>
2020-05-13 17:03:50 +0700

    a

--- config/app.php
+++ config/app.php
@@ -162,8 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
-        Way\Generators\GeneratorsServiceProvider::class,
-Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
         /*
          * Package Service Providers...
          */
